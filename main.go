6f7593f0054a50e1aa94e82f06f6273112979b74
byxor <brandnewbyxor@gmail.com>
2018-07-05 22:28:51 +0100

    Remove code

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-}
-
-func Decompile(bytes []byte) string {
-	if len(bytes) == 0 {
-		return ""
-	} else {
-		switch bytes[0] {
-		case 0x01:
-			return ";"
-		case 0x16:
-			return fmt.Sprintf("#%08x", binary.LittleEndian.Uint32(bytes[1:]))
-		case 0x17:
-			return fmt.Sprint(int32(binary.LittleEndian.Uint32(bytes[1:])))
-		}
-		return ""
-	}
 }
