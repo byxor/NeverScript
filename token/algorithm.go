3a90263d1856e5dfbe762f37204e12a37e22c2ef
byxor <brandnewbyxor@gmail.com>
2018-07-07 00:36:26 +0100

    Refactor

--- token/algorithm.go
+++ token/algorithm.go
@@ -30,55 +30,49 @@ type constructor struct {
 /* The constructor functions are checked in order.
  * The ordering is important! */
 var constructors = []constructor{
-	{EndOfFile, newSingleByteChecker(0x00)},
-	{EndOfLine, newSingleByteChecker(0x01)},
-	{StartOfStruct, newSingleByteChecker(0x03)},
-	{EndOfStruct, newSingleByteChecker(0x04)},
-	{StartOfArray, newSingleByteChecker(0x05)},
-	{EndOfArray, newSingleByteChecker(0x06)},
-	{Assignment, newSingleByteChecker(0x07)},
-	{EqualityCheck, newSingleByteChecker(0x11)},
-	{LessThanCheck, newSingleByteChecker(0x12)},
-	{LessThanOrEqualCheck, newSingleByteChecker(0x13)},
-	{GreaterThanCheck, newSingleByteChecker(0x14)},
-	{GreaterThanOrEqualCheck, newSingleByteChecker(0x15)},
-	{Subtraction, newSingleByteChecker(0x0A)},
-	{Addition, newSingleByteChecker(0x0B)},
-	{Division, newSingleByteChecker(0x0C)},
-	{Multiplication, newSingleByteChecker(0x0D)},
-	{Break, newSingleByteChecker(0x22)},
-	{StartOfFunction, newSingleByteChecker(0x23)},
-	{EndOfFunction, newSingleByteChecker(0x24)},
-	{StartOfIf, newSingleByteChecker(0x25)},
-	{Else, newSingleByteChecker(0x26)},
-	{ElseIf, newSingleByteChecker(0x27)},
-	{EndOfIf, newSingleByteChecker(0x28)},
-	{Return, newSingleByteChecker(0x29)},
-	{Name, isName},
-	{Integer, isInteger},
-	{Float, isFloat},
+	{EndOfFile, requirePrefix(0x00)},
+	{EndOfLine, requirePrefix(0x01)},
+	{StartOfStruct, requirePrefix(0x03)},
+	{EndOfStruct, requirePrefix(0x04)},
+	{StartOfArray, requirePrefix(0x05)},
+	{EndOfArray, requirePrefix(0x06)},
+	{Assignment, requirePrefix(0x07)},
+	{EqualityCheck, requirePrefix(0x11)},
+	{LessThanCheck, requirePrefix(0x12)},
+	{LessThanOrEqualCheck, requirePrefix(0x13)},
+	{GreaterThanCheck, requirePrefix(0x14)},
+	{GreaterThanOrEqualCheck, requirePrefix(0x15)},
+	{Subtraction, requirePrefix(0x0A)},
+	{Addition, requirePrefix(0x0B)},
+	{Division, requirePrefix(0x0C)},
+	{Multiplication, requirePrefix(0x0D)},
+	{Break, requirePrefix(0x22)},
+	{StartOfFunction, requirePrefix(0x23)},
+	{EndOfFunction, requirePrefix(0x24)},
+	{StartOfIf, requirePrefix(0x25)},
+	{Else, requirePrefix(0x26)},
+	{ElseIf, requirePrefix(0x27)},
+	{EndOfIf, requirePrefix(0x28)},
+	{Return, requirePrefix(0x29)},
+	{Name, requirePrefixAndLength(0x16, 5)},
+	{Integer, requirePrefixAndLength(0x17, 5)},
+	{Float, requirePrefixAndLength(0x1A, 5)},
 	{ChecksumTableEntry, isCheckSumTableEntry},
-}
-
-func isName(bytes []byte) bool {
-	return newSingleByteChecker(0x16)(bytes) && len(bytes) == 5
-}
-
-func isInteger(bytes []byte) bool {
-	return newSingleByteChecker(0x17)(bytes) && len(bytes) == 5
-}
-
-func isFloat(bytes []byte) bool {
-	return newSingleByteChecker(0x1A)(bytes) && len(bytes) == 5
 }
 
 func isCheckSumTableEntry(bytes []byte) bool {
 	isLongEnough := len(bytes) > 6
 	isNullTerminated := bytes[len(bytes)-1] == 0
-	return newSingleByteChecker(0x2B)(bytes) && isLongEnough && isNullTerminated
+	return requirePrefix(0x2B)(bytes) && isLongEnough && isNullTerminated
 }
 
-func newSingleByteChecker(n byte) func([]byte) bool {
+func requirePrefixAndLength(prefix byte, length int) func([]byte) bool {
+	return func(bytes []byte) bool {
+		return requirePrefix(prefix)(bytes) && len(bytes) == length
+	}
+}
+
+func requirePrefix(n byte) func([]byte) bool {
 	return func(bytes []byte) bool {
 		return bytes[0] == n
 	}
