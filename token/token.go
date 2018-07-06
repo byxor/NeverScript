8f3a0620cd2a142c20c892ef3c6a57a5246b60ac
byxor <brandnewbyxor@gmail.com>
2018-07-06 22:35:43 +0100

    Refactor

--- token/token.go
+++ token/token.go
@@ -59,67 +59,42 @@ const (
 // -----------------------------
 
 type constructor struct {
-	function func([]byte) bool
 	token    Token
+	function func([]byte) bool
 }
 
 /* The constructor functions are checked in order.
  * The ordering is important! */
 var constructors = []constructor{
-	{isEndOfFile, EndOfFile},
-	{isEndOfLine, EndOfLine},
-	{isAssignment, Assignment},
-	{isEqualityCheck, EqualityCheck},
-	{isLessThanCheck, LessThanCheck},
-	{isLessThanOrEqualCheck, LessThanOrEqualCheck},
-	{isGreaterThanCheck, GreaterThanCheck},
-	{isGreaterThanOrEqualCheck, GreaterThanOrEqualCheck},
-	{isSubtraction, Subtraction},
-	{isAddition, Addition},
-	{isDivision, Division},
-	{isMultiplication, Multiplication},
-	{isStartOfStruct, StartOfStruct},
-	{isEndOfStruct, EndOfStruct},
-	{isStartOfArray, StartOfArray},
-	{isEndOfArray, EndOfArray},
-	{isStartOfIf, StartOfIf},
-	{isElse, Else},
-	{isElseIf, ElseIf},
-	{isEndOfIf, EndOfIf},
-	{isName, Name},
-	{isInteger, Integer},
-	{isFloat, Float},
-	{isStartOfFunction, StartOfFunction},
-	{isEndOfFunction, EndOfFunction},
-	{isReturn, Return},
-	{isBreak, Break},
-	{isCheckSumTableEntry, ChecksumTableEntry},
+	{EndOfFile, singleByte(0x00)},
+	{EndOfLine, singleByte(0x01)},
+	{StartOfStruct, singleByte(0x03)},
+	{EndOfStruct, singleByte(0x04)},
+	{StartOfArray, singleByte(0x05)},
+	{EndOfArray, singleByte(0x06)},
+	{Assignment, singleByte(0x07)},
+	{EqualityCheck, singleByte(0x11)},
+	{LessThanCheck, singleByte(0x12)},
+	{LessThanOrEqualCheck, singleByte(0x13)},
+	{GreaterThanCheck, singleByte(0x14)},
+	{GreaterThanOrEqualCheck, singleByte(0x15)},
+	{Subtraction, singleByte(0x0A)},
+	{Addition, singleByte(0x0B)},
+	{Division, singleByte(0x0C)},
+	{Multiplication, singleByte(0x0D)},
+	{Break, singleByte(0x22)},
+	{StartOfFunction, singleByte(0x23)},
+	{EndOfFunction, singleByte(0x24)},
+	{StartOfIf, singleByte(0x25)},
+	{Else, singleByte(0x26)},
+	{ElseIf, singleByte(0x27)},
+	{EndOfIf, singleByte(0x28)},
+	{Return, singleByte(0x29)},
+	{Name, isName},
+	{Integer, isInteger},
+	{Float, isFloat},
+	{ChecksumTableEntry, isCheckSumTableEntry},
 }
-
-var isEndOfFile = singleByte(0x00)
-var isEndOfLine = singleByte(0x01)
-var isStartOfStruct = singleByte(0x03)
-var isEndOfStruct = singleByte(0x04)
-var isStartOfArray = singleByte(0x05)
-var isEndOfArray = singleByte(0x06)
-var isAssignment = singleByte(0x07)
-var isStartOfFunction = singleByte(0x23)
-var isEndOfFunction = singleByte(0x24)
-var isStartOfIf = singleByte(0x25)
-var isElse = singleByte(0x26)
-var isElseIf = singleByte(0x27)
-var isEndOfIf = singleByte(0x28)
-var isReturn = singleByte(0x29)
-var isSubtraction = singleByte(0x0A)
-var isAddition = singleByte(0x0B)
-var isDivision = singleByte(0x0C)
-var isMultiplication = singleByte(0x0D)
-var isEqualityCheck = singleByte(0x11)
-var isLessThanCheck = singleByte(0x12)
-var isLessThanOrEqualCheck = singleByte(0x13)
-var isGreaterThanCheck = singleByte(0x14)
-var isGreaterThanOrEqualCheck = singleByte(0x15)
-var isBreak = singleByte(0x22)
 
 func isName(bytes []byte) bool {
 	return singleByte(0x16)(bytes) && len(bytes) == 5
