6e715ef081fd4a59a77ab1b215132d2085803ecb
byxor <brandnewbyxor@gmail.com>
2018-07-13 18:25:55 +0100

    Add special thanks

---
+++ thanks.go
@@ -0,0 +1,14 @@
+package main
+
+// These people made this project possible.
+var people = []string{
+	"gone",
+	"Source",
+	"Edem",
+	"Biz",
+	"darkdks",
+	"IronHawk",
+	"Sk8ace",
+	"Chase",
+	"Morten",
+}
