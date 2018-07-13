0161dc0012e37b999a155a32ec30c0da1492a7fe
byxor <brandnewbyxor@gmail.com>
2018-07-13 02:29:08 +0100

    Improve arrays

--- code/generation.go
+++ code/generation.go
@@ -37,23 +37,29 @@ func generateUsing(state stateMap) string {
 
 	if evaluator, ok := evaluators[token.Type]; ok {
 		result := evaluator(state)
+		result = makeStatefulAdjustments(result, state)
 		state["index"] = index + 1
-
-		if state["inArray"].(int) > 0 {
-			inRange := index < len(tokens)-1 && index > 0
-			if inRange {
-				notFirstElement := token.Type != StartOfArray
-				notLastElement := tokens[index+1].Type != EndOfArray
-				if notFirstElement && notLastElement {
-					result += " "
-				}
-			}
-		}
-
 		return result + generateUsing(state)
 	} else {
 		panic(fmt.Sprintf("No evaluator found for %s tokens!", tokens[0].Type.String()))
 	}
+}
+
+func makeStatefulAdjustments(result string, state stateMap) string {
+	tokens := state["tokens"].([]Token)
+	token := state["token"].(Token)
+	index := state["index"].(int)
+	if state["inArray"].(int) > 0 {
+		inRange := index < len(tokens)-1 && index > 0
+		if inRange {
+			notFirstElement := token.Type != StartOfArray
+			notLastElement := tokens[index+1].Type != EndOfArray
+			if notFirstElement && notLastElement {
+				result += " "
+			}
+		}
+	}
+	return result
 }
 
 type stateMap map[string]interface{}
