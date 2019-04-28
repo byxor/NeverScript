c44d6d87cf76d42a281cd6b59944e78769a0e784
byxor <brandnewbyxor@gmail.com>
2019-04-28 21:12:48 +0100

    Add cool artwork

--- main.go
+++ main.go
@@ -8,12 +8,27 @@ import (
 	"github.com/byxor/NeverScript/compiler"
 )
 
+const (
+    banner = `
+     __                    __           _       _   
+  /\ \ \_____   _____ _ __/ _\ ___ _ __(_)_ __ | |_ 
+ /  \/ / _ \ \ / / _ \ '__\ \ / __| '__| | '_ \| __|
+/ /\  /  __/\ V /  __/ |  _\ \ (__| |  | | |_) | |_ 
+\_\ \/ \___| \_/ \___|_|  \__/\___|_|  |_| .__/ \__|
+                                         |_|        
+`
+)
+
 func main() {
 	arguments := parseCommandLineArguments()
 
-	fmt.Println("Welcome to NeverScript")
+	fmt.Println(banner)
+
+	argumentsWereSupplied := false
 
 	if *arguments.FileToCompile != "" {
+		argumentsWereSupplied = true
+
 		fmt.Printf("Compiling '%s'... ", *arguments.FileToCompile)
 
 		data, err := ioutil.ReadFile(*arguments.FileToCompile)
@@ -30,14 +45,16 @@ func main() {
 	}
 
 	if *arguments.FileToDecompile != "" {
+		argumentsWereSupplied = true
+
 		fmt.Printf("Compiling '%s'... ", *arguments.FileToDecompile)
-
-		// check(err)
-
+		fmt.Print("(not implemented yet)")
 		fmt.Println("done")
 	}
 
-	fmt.Println("Goodbye!")
+	if !argumentsWereSupplied {
+		flag.Usage()
+	}
 }
 
 type commandLineArguments struct {
@@ -47,8 +64,8 @@ type commandLineArguments struct {
 
 func parseCommandLineArguments() commandLineArguments {
 	args := commandLineArguments {
-		FileToCompile:   flag.String("c", "", "A .ns file to compile."),
-		FileToDecompile: flag.String("d", "", "A .qb file to decompile."),
+		FileToCompile:   flag.String("c", "", "Specify a file to compile (.ns)."),
+		FileToDecompile: flag.String("d", "", "Specify a file to decompile (.qb)."),
 	}
 	flag.Parse()
 	return args
